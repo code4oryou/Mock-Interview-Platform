943902e3136ee26c19cbf8d3b2d093fff401e5b7
Ayush Mandal <ayushmandal0122@gmail.com>
2025-05-15 00:10:41 +0530

    added auth and root routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const monaSans = Mona_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "WisePrep",
+  title: "Mockly",
   description: "An AI-powered platform for preparing for mock interviews.",
 };
 
