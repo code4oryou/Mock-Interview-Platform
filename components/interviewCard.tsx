ac46610889fa8e18258b7c5009635fba1a389ff8
Ayush Mandal <ayushmandal0122@gmail.com>
2025-05-19 00:10:43 +0530

    home page initial setup

---
+++ components/interviewCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt} : InterviewCardProps) => {
+  const feedback = null as Feedback | null;
+  const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
+
+  return (
+    <div>InterviewCard</div>
+  );
+};
+
+export default InterviewCard;
